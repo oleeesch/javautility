9e5c63075d0b3ccf39659511256b9936f9dc36bd
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-27 17:09:53 +0200

    Add @DeprecatedSince annotation
    Introduce @DeprecatedSince annotation to mark deprecated elements with a specified version. This allows for better tracking and documentation of deprecated elements in the codebase. The annotation can be applied to various elements such as constructors, fields, methods, and classes.

---
+++ common-utils/src/main/java/dev/ole/utility/annotations/DeprecatedSince.java
@@ -0,0 +1,14 @@
+package dev.ole.utility.annotations;
+
+import javax.annotation.Nonnull;
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.PACKAGE, ElementType.PARAMETER, ElementType.TYPE})
+public @interface DeprecatedSince {
+
+    @Nonnull
+    String value();
+
+}
